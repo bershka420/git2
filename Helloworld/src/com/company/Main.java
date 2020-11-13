c9ec40b162eed33172566420b116659f0d2740aa
ASUS <bershka420bershka@gmail.com>
2020-11-13 16:52:23 +0300

    Commit 1

---
+++ Helloworld/src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+//        String str4 = new String(new char[]{'w', 'e', 'l', 'c', 'o', 'm', 'e'}, 2, 4);
+//        System.out.println(str4);
+        // Scanner sc = new Scanner(System.in);
+        //     String str = sc.nextLine();
+        /* System.out.println(str); */
+        //int num = Integer.parseInt(sc.nextLine());
+       // System.out.println(num + 2);
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        String str = br.readLine();
+//        int num = Integer.parseInt(str);
+//        System.out.println(num +2 );
+//
+//        int[] array = new int[] { 1, 2, 2, 4, 5 };
+//        for (int i = 0; i < array.length; i++){
+//            System.out.println(array[i]);
+        //int[] nums = {1, 2, 3, 4, 5, 9};
+
+       // int last = nums[nums.length-1];
+       // System.out.println(last);
+        int[] nums = {1,2,3,4,6,7};
+        for(int unt : nums){
+        System.out.println(unt);
+        }
+        }}
+
+
+
+
