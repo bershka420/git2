cd6b9c6a9c4c3338262fb0cdeafb3e8c691a08c3
ASUS <bershka420bershka@gmail.com>
2020-11-15 13:42:29 +0300

    Commit 1

---
+++ Helloworld/src/com/company/Anima.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public enum Anima {
+    DOG,CAT,FROG
+}
--- Helloworld/src/com/company/Main.java
+++ Helloworld/src/com/company/Main.java
@@ -1,11 +1,13 @@
 package com.company;
 
 
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
+
   // int[][] nums= new int[][]{ {1,3,3},{4,5,6},{7,8,9}};
 
   // for(int i=0; i < nums.length; i++){
@@ -13,72 +15,84 @@ public class Main {
  //               System.out.println(nums[i][k]);
   //          }
   //      }
-
-        Scanner in = new Scanner(System.in);
-        System.out.println("vvedite chisilo");
-        int num=in.nextInt();
-        switch (num){
-            case 1:
-                System.out.println("вы ввели 1");
-                break;
-            case 2:
-                System.out.println("вы ввели 2");
-                break;
-            case 3:
-                System.out.println("вы ввели 3");
-                break;
-            case 4:
-                System.out.println("вы ввели 4");
-                break;
-            case 5:
-                System.out.println("вы ввели 5");
-                break;
-            case 6:
-                System.out.println("вы ввели 6");
-                break;
-            case 7:
-                System.out.println("вы ввели 7");
-                break;
-            case 8:
-                System.out.println("вы ввели 8");
-                break;
-            case 9:
-                System.out.println("вы ввели 9");
-                break;
-            case 10:
-                System.out.println("вы ввели 10");
-                break;
-            case 11:
-                System.out.println("вы ввели 11");
-                break;
-            case 12:
-                System.out.println("вы ввели 12");
-                break;
-            case 13:
-                System.out.println("вы ввели 13");
-                break;
-            case 14:
-                System.out.println("вы ввели 14");
-                break;
-            case 15:
-                System.out.println("вы ввели 15");
-                break;
-            case 16:
-                System.out.println("вы ввели 16");
-                break;
-            case 17:
-                System.out.println("вы ввели 17");
-                break;
-            case 18:
-                System.out.println("вы ввели 18");
-                break;
-            case 19:
-                System.out.println("вы ввели 19");
-                break;
-
-        }
-        //System.out.printf("vi vveli %d \n", num);
-       in.close();
+//________________________ my one programm--------------------
+//        Scanner in = new Scanner(System.in);
+//        System.out.println("vvedite chisilo");
+//        int num=in.nextInt();
+//        switch (num){
+//            case 1:
+//                System.out.println("вы ввели 1");
+//                break;
+//            case 2:
+//                System.out.println("вы ввели 2");
+//                break;
+//            case 3:
+//                System.out.println("вы ввели 3");
+//                break;
+//            case 4:
+//                System.out.println("вы ввели 4");
+//                break;
+//            case 5:
+//                System.out.println("вы ввели 5");
+//                break;
+//            case 6:
+//                System.out.println("вы ввели 6");
+//                break;
+//            case 7:
+//                System.out.println("вы ввели 7");
+//                break;
+//            case 8:
+//                System.out.println("вы ввели 8");
+//                break;
+//            case 9:
+//                System.out.println("вы ввели 9");
+//                break;
+//            case 10:
+//                System.out.println("вы ввели 10");
+//                break;
+//            case 11:
+//                System.out.println("вы ввели 11");
+//                break;
+//            case 12:
+//                System.out.println("вы ввели 12");
+//                break;
+//            case 13:
+//                System.out.println("вы ввели 13");
+//                break;
+//            case 14:
+//                System.out.println("вы ввели 14");
+//                break;
+//            case 15:
+//                System.out.println("вы ввели 15");
+//                break;
+//            case 16:
+//                System.out.println("вы ввели 16");
+//                break;
+//            case 17:
+//                System.out.println("вы ввели 17");
+//                break;
+//            case 18:
+//                System.out.println("вы ввели 18");
+//                break;
+//            case 19:
+//                System.out.println("вы ввели 19");
+//                break;
+//        }
+//        System.out.printf("vi vveli %d \n", num);
+//       in.close();
+// -------------------------
+//        Anima anima = Anima.FROG;
+//        switch (anima){
+//            case CAT:
+//                System.out.println("кошка");
+//                break;
+//            case DOG:
+//                System.out.println("собака");
+//                break;
+//            case FROG:
+//                System.out.println("ЛЯГУШКА");
+//                break;
+//        }
     }
 }
 
